fbb2259ebc8735577311f7d5dff8acf37e0bbd77
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-10-05 19:45:24 -0400

    adding working TDD and renderWig function

---
+++ products/products.js
@@ -0,0 +1,14 @@
+import { wigs } from '../data.js';
+import { renderWig } from '../utils.js';
+
+// In your products.js, you will need to:
+
+// import your data and your DOM generation function
+// locate the list element where your products will go
+// loop through your data
+// Create a variable that is the singular of your domain list and assign based on the current array index. For example, const fruit = fruits[i];
+// Pass to your DOM generation function and capture result in variable
+// Append to the top-level list element
+// git add, commit, push!
+
+// THIS WILL HAVE THE FOR LOOP. 
